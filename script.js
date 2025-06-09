328a2987bc3b990c92aee81b429f0d3a1d81ae47
Samir Katakamsetty <katakamsettysamir@gmail.com>
2025-06-08 21:20:52 -0400

    Added quiz questions

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-// The DOM Elements
+/* The DOM Elements */ 
 
 // Screens
 const startScreen = document.getElementById("start-screen");
@@ -8,14 +8,74 @@ const resultsScreen = document.getElementById("results-screen");
 
 // Buttons
 const startButton = document.getElementById("start-button");
+const restartButton = document.getElementById("restart");
 
 // Questions & Changing Text
 const textForQuestion = document.getElementById("textForQuestion");
 const currentQuestionNumber = document.getElementById("currentQuestionNumber");
+const totalQuestions = document.getElementById("totalQuestions");
+
 
 // Progress Bar
 const progressBar = document.getElementById("progress-bar"); 
 const progress = document.getElementById("progress"); 
+
+// Info
+const resultsInfo = document.getElementById("results-info");
+const closingMessage = document.getElementById("closing-message");
+const quizScore = document.getElementById("score");
+const correctAnswers = document.getElementById("correct-answers");
+
+
+/* Store the quiz questions as an array */ 
+
+// Array of Quiz Questions for the Quiz
+
+const questions = [
+
+    // Question #1
+    {
+        question: "What is the creator's name?",
+        answers: [
+            {answertext: "Samir", isCorrect: true },
+            {answertext: "Samira", isCorrect: false },
+            {answertext: "Samor", isCorrect: false},
+            {answertext: "Sameer", isCorrect: false},
+        ]
+    },
+    // Question 2
+    {
+        question: "What is Samir's favorite food?",
+        answers: [
+            {answertext: "Pizza", isCorrect: true},
+            {answertext: "Sushi", isCorrect: true },
+            {answertext: "Asparagus", isCorrect: false},
+            {answertext: "Green Beans", isCorrect: true },
+        ]
+    },
+    // question 3
+    {
+        question: "What is my favorite language?",
+        answers: [
+            {answertext: "Python", isCorrect: false},
+            {answertext: "C", isCorrect: true},
+            {answertext: "Java", isCorrect: false},
+            {answertext: "C#", isCorrect: false}
+        ]
+    }
+]
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
