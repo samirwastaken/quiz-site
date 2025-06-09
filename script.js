84370d96d71109e56d11007d3a822f71ae9019d0
Samir Katakamsetty <katakamsettysamir@gmail.com>
2025-06-08 20:29:52 -0400

    added DOM to JS file

---
+++ script.js
@@ -0,0 +1,23 @@
+
+// The DOM Elements
+
+// Screens
+const startScreen = document.getElementById("start-screen");
+const quizScreen = document.getElementById("quizScreen");
+const resultsScreen = document.getElementById("results-screen");
+
+// Buttons
+const startButton = document.getElementById("start-button");
+
+// Questions & Changing Text
+const textForQuestion = document.getElementById("textForQuestion");
+const currentQuestionNumber = document.getElementById("currentQuestionNumber");
+
+// Progress Bar
+const progressBar = document.getElementById("progress-bar"); 
+const progress = document.getElementById("progress"); 
+
+
+
+
+
